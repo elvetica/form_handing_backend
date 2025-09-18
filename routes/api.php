58cad32bf5ef69984ef81d6a217101dd3ca50ee0
Paul Lindquist <paul.lindquist@gmail.com>
2025-09-18 08:02:56 -0500

    add throttling

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormSubmissionController;
 
-Route::post('/forms/submit', [FormSubmissionController::class, 'store']);
-// Temporary GET route for debugging
-Route::get('/forms/submit', function () {
-    return response()->json(['message' => 'GET route is working!!!!!!!']);
+Route::middleware('throttle:10,1')->group(function () {
+    Route::post('/forms/submit', [FormSubmissionController::class, 'store']);
 });
