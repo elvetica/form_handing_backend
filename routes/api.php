484748ed08c8b4202adc97f6df722bdd9ca656ff
Paul Lindquist <paul.lindquist@gmail.com>
2025-09-21 20:57:06 -0500

    Add debug route

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,7 @@ use App\Http\Controllers\FormSubmissionController;
 
 Route::middleware('throttle:10,1')->group(function () {
     Route::post('/forms/submit', [FormSubmissionController::class, 'store']);
+    Route::get('/forms/submit', function () {
+        return response()->json(['message' => 'GET route is working!!!!!!!']);
+    });
 });
